dc7d9ddddd710e76e833869c4ea051b89900f43c
Rohit-Magar <rohitmagar084@gmail.com>
2021-07-16 23:08:11 +0530

    Greetings program added

---
+++ Yourname.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cout<<"Your name: ";
+    getline(cin, s);
+    cout<<"Nice to see you "<<s<<"!";
+    cout<<endl;
+    cout<<"Have a nice day.";
+
+    return 0;
+}
